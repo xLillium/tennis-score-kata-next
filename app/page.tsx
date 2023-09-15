93e4fb0166d82184a3bf8bdbd9a27d0acd391f2e
xLillium <nicolas.motillon@gmail.com>
2023-09-15 18:55:17 +0200

    Add implementation for initial score test

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import styles from './page.module.css'
 export default function TennisGame() {
     return (
         <main className={styles.main}>
+            <div>0 - 0</div>
         </main>
     )
 }
