79bdc38699b4f4e580ed7e230a49ef23417f422c
xLillium <nicolas.motillon@gmail.com>
2023-09-15 19:32:48 +0200

    Add new test to update Player 1 score

--- __tests__/TennisGame.test.js
+++ __tests__/TennisGame.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import TennisGame from '../app/page';
 
@@ -6,6 +6,12 @@ describe('TennisGame', () => {
     it('should render the initial score 0-0', () => {
         const { getByText } = render(<TennisGame />);
         expect(getByText('0 - 0')).toBeInTheDocument();
+    });
+
+    it('should update the score for player 1', () => {
+        const { getByText } = render(<TennisGame />);
+        fireEvent.click(getByText('+1 Point for Player 1'));
+        expect(getByText('15 - 0')).toBeInTheDocument();
     });
 });
 
