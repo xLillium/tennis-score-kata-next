60f946e79cdb29df7797ae86a9da8e8cf2a9c3d2
xLillium <nicolas.motillon@gmail.com>
2023-09-15 20:36:24 +0200

    Add cases when Player 2 scores up until winning game

--- __tests__/TennisGame.test.js
+++ __tests__/TennisGame.test.js
@@ -32,10 +32,41 @@ describe('TennisGame', () => {
         expect(getByText('Game Player 1')).toBeInTheDocument();
     });
 
+
+    it('should display "0 - 15" when Player 2 scores', () => {
+        const { getByText } = render(<TennisGame />);
+        pointForPlayer2(1, getByText);
+        expect(getByText('0 - 15')).toBeInTheDocument();
+    });
+
+    it('should display "0 - 30" when Player 2 scores twice', () => {
+        const { getByText } = render(<TennisGame />);
+        pointForPlayer2(2, getByText);
+        expect(getByText('0 - 30')).toBeInTheDocument();
+    });
+
+    it('should display "0 - 40" when Player 2 scores thrice', () => {
+        const { getByText } = render(<TennisGame />);
+        pointForPlayer2(3, getByText);
+        expect(getByText('0 - 40')).toBeInTheDocument();
+    });
+
+    it('should display "Game Player 2" when Player 2 scores after 0-40', () => {
+        const { getByText } = render(<TennisGame />);
+        pointForPlayer2(4, getByText);
+        expect(getByText('Game Player 2')).toBeInTheDocument();
+    });
+
 });
 
 function pointForPlayer1(times, getByText) {
     for (let i = 0; i < times; i++) {
         fireEvent.click(getByText('+1 Point for Player 1'));
+    }
+}
+
+function pointForPlayer2(times, getByText) {
+    for (let i = 0; i < times; i++) {
+        fireEvent.click(getByText('+1 Point for Player 2'));
     }
 }
--- app/TennisGameClient.tsx
+++ app/TennisGameClient.tsx
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 
 export function TennisGameClient() {
     const [player1Score, setPlayer1Score] = useState(0);
+    const [player2Score, setPlayer2Score] = useState(0);
     const [gameState, setGameState] = useState("");
 
     const givePointToPlayer1 = () => {
@@ -19,12 +20,24 @@ export function TennisGameClient() {
         }
     }
 
+    const givePointToPlayer2 = () => {
+        if (player2Score === 0) {
+            setPlayer2Score(15);
+        } else if (player2Score === 15) {
+            setPlayer2Score(30);
+        } else if (player2Score === 30) {
+            setPlayer2Score(40);
+        } else {
+            setGameState("Game Player 2");
+        }
+    }
     return (
         <div>
             <div>
-                {gameState ? gameState : `${player1Score} - 0`}
+                {gameState ? gameState : `${player1Score} - ${player2Score}`}
             </div>
             <button onClick={givePointToPlayer1}>+1 Point for Player 1</button>
+            <button onClick={givePointToPlayer2}>+1 Point for Player 2</button>
         </div>
     );
 }
