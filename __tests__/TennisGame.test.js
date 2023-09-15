d35a12c79c8b6a2c9b3856dd70d291e08a4df9df
xLillium <nicolas.motillon@gmail.com>
2023-09-15 20:23:14 +0200

    Refactor test file

--- __tests__/TennisGame.test.js
+++ __tests__/TennisGame.test.js
@@ -3,40 +3,39 @@ import '@testing-library/jest-dom'
 import TennisGame from '../app/page';
 
 describe('TennisGame', () => {
-    it('should render the initial score 0-0', () => {
+    it('should display "0 - 0" when game starts', () => {
         const { getByText } = render(<TennisGame />);
         expect(getByText('0 - 0')).toBeInTheDocument();
     });
 
-    it('should update the score to 15 - 0 when player 1 scores', () => {
+    it('should display "15 - 0" when Player 1 scores', () => {
         const { getByText } = render(<TennisGame />);
-        fireEvent.click(getByText('+1 Point for Player 1'));
+        pointForPlayer1(1, getByText);
         expect(getByText('15 - 0')).toBeInTheDocument();
     });
 
-    it('should update the score to 30 - 0 when player 1 scores twice', () => {
+    it('should display "30 - 0" when Player 1 scores twice', () => {
         const { getByText } = render(<TennisGame />);
-        fireEvent.click(getByText('+1 Point for Player 1'));
-        fireEvent.click(getByText('+1 Point for Player 1'));
+        pointForPlayer1(2, getByText);
         expect(getByText('30 - 0')).toBeInTheDocument();
     });
 
-    it('should update the score to 40 - 0 when player 1 scores thrice', () => {
+    it('should display "40 - 0" when Player 1 scores thrice', () => {
         const { getByText } = render(<TennisGame />);
-        fireEvent.click(getByText('+1 Point for Player 1'));
-        fireEvent.click(getByText('+1 Point for Player 1'));
-        fireEvent.click(getByText('+1 Point for Player 1'));
+        pointForPlayer1(3, getByText);
         expect(getByText('40 - 0')).toBeInTheDocument();
     });
 
     it('should display "Game Player 1" when Player 1 scores after 40-0', () => {
         const { getByText } = render(<TennisGame />);
-        fireEvent.click(getByText('+1 Point for Player 1'));
-        fireEvent.click(getByText('+1 Point for Player 1'));
-        fireEvent.click(getByText('+1 Point for Player 1'));
-        fireEvent.click(getByText('+1 Point for Player 1'));
+        pointForPlayer1(4, getByText);
         expect(getByText('Game Player 1')).toBeInTheDocument();
     });
 
 });
 
+function pointForPlayer1(times, getByText) {
+    for (let i = 0; i < times; i++) {
+        fireEvent.click(getByText('+1 Point for Player 1'));
+    }
+}
